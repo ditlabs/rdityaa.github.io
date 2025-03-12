3e6e4057ee758e4c111c3b42adbe00fd59d075c3
rdityaa <radityarizki2309@gmail.com>
2025-03-13 02:33:56 +0700

    add navbar with hamburger menu

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 // Hamburger Menu
 const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector
+('#nav-menu');
 
 hamburger.addEventListener('click', function () {
     hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
 });
 
 // Navbar
